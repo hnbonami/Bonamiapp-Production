4295172cfba44a5e9f00545d59c4c87f35103eba
Hannes Bonami <hannesbonami@hotmail.com>
2025-10-15 00:04:31 +0200

    Add nieuw logging systeem

--- config/services.php
+++ config/services.php
@@ -18,6 +18,10 @@ return [
         'token' => env('POSTMARK_TOKEN'),
     ],
 
+    'openai' => [
+        'key' => env('OPENAI_API_KEY'),
+    ],
+
     'resend' => [
         'key' => env('RESEND_KEY'),
     ],
