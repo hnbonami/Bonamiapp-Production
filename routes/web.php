32683f553e4b5068bb9f2bc786401c89e62799f2
Hannes Bonami <hannesbonami@hotmail.com>
2025-10-26 22:33:09 +0100

    werkende bikefir en klanten import tool

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,22 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DashboardStatsController;
+
+// DASHBOARD WIDGET SYSTEEM
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+    Route::get('/dashboard/widgets/create', [DashboardController::class, 'create'])->name('dashboard.widgets.create');
+    Route::post('/dashboard/widgets', [DashboardController::class, 'store'])->name('dashboard.widgets.store');
+    Route::get('/dashboard/widgets/{widget}/edit', [DashboardController::class, 'edit'])->name('dashboard.widgets.edit');
+    Route::put('/dashboard/widgets/{widget}', [DashboardController::class, 'update'])->name('dashboard.widgets.update');
+    Route::post('/dashboard/widgets/layout', [DashboardController::class, 'updateLayout'])->name('dashboard.widgets.updateLayout');
+    Route::post('/dashboard/widgets/{widget}/toggle', [DashboardController::class, 'toggleVisibility'])->name('dashboard.widgets.toggle');
+    Route::delete('/dashboard/widgets/{widget}', [DashboardController::class, 'destroy'])->name('dashboard.widgets.destroy');
+    Route::get('/dashboard/stats/live', [DashboardStatsController::class, 'getLiveStats'])->name('dashboard.stats.live');
+    Route::get('/dashboard/stats/widget', [DashboardStatsController::class, 'getWidgetData'])->name('dashboard.stats.widget');
+});
+
 // SJABLOON-MANAGER ROUTE - NAAR TEMPLATES INDEX BLADE!
 Route::get('/sjabloon-manager', function() {
     // Get templates data (assuming you have a Template model)
