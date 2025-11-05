af86dbd785cb35d27acd4ac663e47c521dcd6d3b
Hannes Bonami <hannesbonami@hotmail.com>
2025-11-05 21:42:08 +0100

    werkende bikefir en klanten import tool

--- resources/views/klanten/create.blade.php
+++ resources/views/klanten/create.blade.php
@@ -136,8 +136,7 @@
                                name="stad" 
                                id="stad" 
                                value="{{ old('stad') }}" 
-                               class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-                               readonly>
+                               class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
                     </div>
                 </div>
 
