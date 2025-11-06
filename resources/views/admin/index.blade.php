8a37777e079f5e09a23a405c0ac8adb95a36948a
Hannes Bonami <hannesbonami@hotmail.com>
2025-11-06 16:06:23 +0100

    werkende bikefir en klanten import tool

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -194,7 +194,7 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"/>
                     </svg>
                 </div>
-                <h3 class="text-xl font-semibold text-gray-800 ml-3">🎨 Custom Branding</h3>
+                <h3 class="text-xl font-semibold text-gray-800 ml-3">Custom Branding</h3>
             </div>
             <p class="text-gray-600 mb-4">Personaliseer logo's, kleuren en huisstijl voor rapporten en emails. Maak de app helemaal van jou!</p>
             <a href="{{ route('branding.index') }}" class="inline-flex items-center font-medium" style="color: #111;">
