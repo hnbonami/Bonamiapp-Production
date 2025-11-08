9e43098095855b1ad70a19e9d1a11967bc07c4e8
Hannes Bonami <hannesbonami@hotmail.com>
2025-11-08 09:10:46 +0100

    werkende bikefir en klanten import tool

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -11,7 +11,7 @@
             <svg width="16" height="16" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
             </svg>
-            Widget toevoegen
+            <span class="hidden md:inline">Widget toevoegen</span>
         </button>
         @endif
     </div>
